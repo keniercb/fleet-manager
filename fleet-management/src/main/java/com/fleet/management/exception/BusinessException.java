64ab32e91d71131beb931cc2f190839bed9488a4
Z User <z@container>
2026-08-11 19:01:49 +0000

    45e6e0af-9631-41ee-a00d-367c761ffa21

---
+++ fleet-management/src/main/java/com/fleet/management/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.fleet.management.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
