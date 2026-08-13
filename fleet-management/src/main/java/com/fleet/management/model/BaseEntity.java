296f5678527add96a455fd0fb68a1a7d373d201c
Z User <z@container>
2026-08-13 15:30:48 +0000

    feat: agregar @NoArgsConstructor y @AllArgsConstructor a BaseEntity

--- fleet-management/src/main/java/com/fleet/management/model/BaseEntity.java
+++ fleet-management/src/main/java/com/fleet/management/model/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.fleet.management.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder
 @MappedSuperclass
 public abstract class BaseEntity {
